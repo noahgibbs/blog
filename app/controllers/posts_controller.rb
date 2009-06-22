94d68818f84966789d4f68bfde7e3f6e8bc8d74a
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-06-22 10:29:20 -0700

    Add titles to shown posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
   # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
+    @title = @post.title
 
     respond_to do |format|
       format.html # show.html.erb
