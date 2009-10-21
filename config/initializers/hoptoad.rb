623f285542a737c92c5aa397e7c3b5db4d136b60
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-10-21 16:43:15 -0700

    Add hoptoad.rb for error notification

---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,9 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = 'Angelbob's Blog'
+  config.host = 'cheaptoad-catcher.heroku.com'
+  config.port = 80
+
+  config.environment_filters << "BLOG_PASSWORD"
+
+  config.ignore_only = []
+end
