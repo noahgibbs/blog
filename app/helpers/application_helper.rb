55ba610e2789b6c2d483ea476ea0978794d5fc9a
WWW user <www@ubuntu.(none)>
2010-06-29 11:53:20 -0700

    Add 'X days ago' line to posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def time_to_human(time)
+    duration = Time.now - time
+    intervals = [ :year, :month, :week, :day, :hour, :minute ]
+    intervals.each do |interval|
+      if duration > 2.send(interval)
+        return (duration / 1.send(interval)).to_i.to_s + " " +
+          interval.to_s + "s"
+      end
+    end
+    return duration.to_i.to_s + " seconds"
+  end
 end
