19066c889e05de09cd42b5f37db8c737d7aaf4a9
WWW user <www@ubuntu.(none)>
2010-10-30 22:21:42 -0700

    Add slides

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
-  Actions = [:refactor1, :refactor2, :refactor3]
+  Actions = [:refactor1, :refactor2, :refactor3,
+             :slide1, :slide2, :slide3, :slide4, :slide5,
+             :slide6]
 
   caches_action *Actions
 
