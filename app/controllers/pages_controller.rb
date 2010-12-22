18af20163fae785648b2ebc42b8a7e0b16708d7b
Noah Gibbs <noah_gibbs@yahoo.com>
2010-12-21 22:41:57 -0800

    Add actions for refactor slides

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
 
-  Actions = [:refactor1, :refactor2, :refactor3,
-             :slide1, :slide2, :slide3, :slide4, :slide5,
-             :slide6]
+  Actions = [:refactor1, :refactor2, :refactor3] +
+            [:slide1, :slide2, :slide3, :slide4, :slide5, :slide6] +
+            (1..15).map {|i| ("replacing" + i).to_sym}
 
   caches_action *Actions
 
